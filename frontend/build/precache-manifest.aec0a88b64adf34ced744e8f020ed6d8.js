962cbb386d632fe465040f7cacf6eb92c67ee429
Михаил Синяков <pvpshoot@gmail.com>
2019-01-14 12:11:54 +0400

    build

---
+++ frontend/build/precache-manifest.aec0a88b64adf34ced744e8f020ed6d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "154d801aa86d27d20c80",
+    "url": "/static/js/main.154d801a.chunk.js"
+  },
+  {
+    "revision": "79411d98522fe1a7c4e1",
+    "url": "/static/js/1.79411d98.chunk.js"
+  },
+  {
+    "revision": "154d801aa86d27d20c80",
+    "url": "/static/css/main.93c44396.chunk.css"
+  },
+  {
+    "revision": "79411d98522fe1a7c4e1",
+    "url": "/static/css/1.d19bdf61.chunk.css"
+  },
+  {
+    "revision": "19bfa22b6af4aca90477edf2a66ac920",
+    "url": "/index.html"
+  }
+];
